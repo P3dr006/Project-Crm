2d1808ff515f3b69e698112b829ec56bc860149a
P3dr006 <pedrovitorcesario@gmail.com>
2026-04-22 16:58:54 -0300

    feat: implement secure JWT access tokens for user sessions

--- back-end/src/auth_utils.py
+++ back-end/src/auth_utils.py
@@ -1,4 +1,7 @@
 import bcrypt
+import os
+import jwt
+from datetime import datetime, timedelta, timezone
 
 def hash_password(password: str) -> str:
     """Encodes a plain text password into a secure bcrypt hash."""
@@ -15,3 +18,24 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     pwd_bytes = plain_password.encode('utf-8')
     hashed_bytes = hashed_password.encode('utf-8')
     return bcrypt.checkpw(pwd_bytes, hashed_bytes)
+
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+if not SECRET_KEY:
+    raise ValueError("ERRO CRÍTICO: SECRET_KEY não encontrada nas variáveis de ambiente! Verifique seu arquivo .env.")
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict):
+    """Gera um crachá criptografado (JWT) com os dados do usuário e validade."""
+    to_encode = data.copy()
+    
+    # Set the date and time when the badge expires.
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    
+    # Manufacturing the sealed token
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
