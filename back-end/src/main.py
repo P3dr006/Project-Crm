001beaef2d7669e4688d2569aa156d1d2ee479d3
P3dr006 <pedrovitorcesario@gmail.com>
2026-04-27 14:41:54 -0300

    fix: add Vite dev server origins to CORS allowed list

--- back-end/src/main.py
+++ back-end/src/main.py
@@ -25,6 +25,8 @@ app = FastAPI(
 origins = [
     "http://localhost",
     "http://localhost:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
     "http://localhost:5500",
     "http://127.0.0.1:5500",
 ]
