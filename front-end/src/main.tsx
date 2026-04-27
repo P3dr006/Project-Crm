fc2b4ee87fac484beec474b9d8d64640d0a262b1
P3dr006 <pedrovitorcesario@gmail.com>
2026-04-27 15:45:24 -0300

    chore(frontend): update main.tsx to use ReactDOM and React.StrictMode imports

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './index.css' // Crucial for Tailwind v4
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
